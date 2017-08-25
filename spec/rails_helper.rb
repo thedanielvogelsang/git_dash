22455c80e7867824613b5ade4895fe25871e3ef0
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-24 18:40:40 -0600

    creates home and sessions controller, adds link to home index view to get to github oauth login, adds both a root and a callback route in config/routes, adds spec/vcr_github_service_spec.rb to spec file, creates app/services file in prep for github_services service

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'webmock/rspec'
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = "scec/cassettes"
+  config.cassette_library_dir = "spec/cassettes"
   config.hook_into :webmock
 end
 # Add additional requires below this line. Rails is not loaded until this point!
---
+++ spec/services/vcr_github_services_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe GitHubService do
+  describe "users" do
+    before(:each) do
+      @user = User.create(uid:1, username: "thedanielvogelsang", token: )
+    end
+  end
+end
