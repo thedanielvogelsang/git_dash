99621b327c21de3abdf742ba728d2ee8388bb2a5
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-24 23:37:34 -0600

    passing burgeoned organziation model test

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -7,4 +7,17 @@ RSpec.describe Organization, type: :model do
     expect(organization.class).to be(Organization)
     expect(organization.public_members).to eq("https://api.github.com/orgs/1705-BE/public_members")
   end
+  it "can call a valid members url" do
+    organization = Organization.new({:login=>"1705-BE", :id=>31317089, :url=>"https://api.github.com/orgs/1705-BE", :repos_url=>"https://api.github.com/orgs/1705-BE/repos", :events_url=>"https://api.github.com/orgs/1705-BE/events", :hooks_url=>"https://api.github.com/orgs/1705-BE/hooks", :issues_url=>"https://api.github.com/orgs/1705-BE/issues", :members_url=>"https://api.github.com/orgs/1705-BE/members{/member}", :public_members_url=>"https://api.github.com/orgs/1705-BE/public_members{/member}", :avatar_url=>"https://avatars1.githubusercontent.com/u/31317089?v=4", :description=>"1705 BackEnd Group"})
+    expect(organization.name).to eq("1705-BE")
+  end
+  it "can call a valid members url" do
+    organization = Organization.new({:login=>"1705-BE", :id=>31317089, :url=>"https://api.github.com/orgs/1705-BE", :repos_url=>"https://api.github.com/orgs/1705-BE/repos", :events_url=>"https://api.github.com/orgs/1705-BE/events", :hooks_url=>"https://api.github.com/orgs/1705-BE/hooks", :issues_url=>"https://api.github.com/orgs/1705-BE/issues", :members_url=>"https://api.github.com/orgs/1705-BE/members{/member}", :public_members_url=>"https://api.github.com/orgs/1705-BE/public_members{/member}", :avatar_url=>"https://avatars1.githubusercontent.com/u/31317089?v=4", :description=>"1705 BackEnd Group"})
+    expect(organization.class).to be(Organization)
+    expect(organization.repos).to eq("https://api.github.com/orgs/1705-BE/repos")
+  end
+  it "can call a valid members url" do
+    organization = Organization.new({:login=>"1705-BE", :id=>31317089, :url=>"https://api.github.com/orgs/1705-BE", :repos_url=>"https://api.github.com/orgs/1705-BE/repos", :events_url=>"https://api.github.com/orgs/1705-BE/events", :hooks_url=>"https://api.github.com/orgs/1705-BE/hooks", :issues_url=>"https://api.github.com/orgs/1705-BE/issues", :members_url=>"https://api.github.com/orgs/1705-BE/members{/member}", :public_members_url=>"https://api.github.com/orgs/1705-BE/public_members{/member}", :avatar_url=>"https://avatars1.githubusercontent.com/u/31317089?v=4", :description=>"1705 BackEnd Group"})
+    expect(organization.id).to eq(31317089)
+  end
 end
