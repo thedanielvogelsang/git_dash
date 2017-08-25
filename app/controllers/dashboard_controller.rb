72d6b585e7a98177841bf813261bc06955a27218
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-24 19:33:47 -0600

    adds show method for next view pages, splits between my recents and their recents, creates full dashboard with starred repos, followers, followings, repo list, avatar image, and links to open pull requests, my events, their events

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,14 @@ class DashboardController < ApplicationController
     @api = GitHubService.new(current_user)
   end
 
+  def show
+    api = GitHubService.new(current_user)
+    if params['id'] == 'my_feed'
+      @recents = api.my_recents 
+      render 'dashboard/recent_events'
+    elsif params['id'] == 'theirs'
+      @recents = api.their_recents
+      render 'dashboard/network_events'
+    end
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,21 @@
 class SessionsController < ApplicationController
+
   def create
-    @response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['github_client_id']}&client_secret=#{ENV['github_client_secret']}&code=#{params['code']}")
-    token = @response.body.split(/\W+/)[1]
-    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+    response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['github_client_id']}&client_secret=#{ENV['github_client_secret']}&code=#{params['code']}")
+    @token = response.body.split(/\W+/)[1]
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{@token}")
     auth = JSON.parse(oauth_response.body)
-    user = User.find_or_create_by(uid: auth['uid'])
-    user.uid = auth['uid']
+    associate_user(auth)
+    redirect_to dashboard_index_path
+  end
+
+  def associate_user(auth)
+    user = User.find_or_create_by(uid: auth['id'])
+    user.uid = auth['id']
     user.username = auth['login']
-    user.token = token
+    user.token = @token
     user.image = auth['avatar_url']
     user.save
     session[:user_id] = user.id
-    redirect_to dashboard_index_path
   end
 end
