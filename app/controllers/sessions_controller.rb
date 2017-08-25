a9269d85beb5d6b205c53c35dfbcbbd982d70986
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-25 10:26:14 -0600

    stylings on dashboard added and changed, navbar created and utilized, logout function created

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -18,4 +18,10 @@ class SessionsController < ApplicationController
     user.save
     session[:user_id] = user.id
   end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = 'Goodbye'
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "home#index"
   get 'auth/github/callback', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   resources :dashboard, only: [:index, :show]
   get 'open_pull_requests', to: 'dashboard#open_pulls'
 end
