cbb427686093f0c9f6ad53ce7d2882a9af963960
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-24 18:25:10 -0600

    adds validations tests for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates_presence_of :uid,
+                        :username,
+                        :token
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'Validations' do
+    it "is invalid without a uid" do
+      user = User.new(username: "DVOG", token:"5444kadib95n509x", image:"image.url")
+      expect(user).to_not be_valid
+    end
+    it "is invalid without a username" do
+      user = User.new(uid: 123, token:"5444kadib95n509x", image:"image.url")
+      expect(user).to_not be_valid
+    end
+    it "is invalid without a token" do
+      user = User.new(username: "DVOG", uid:123, image:"image.url")
+      expect(user).to_not be_valid
+    end
+    it "is valid without an image" do
+      user = User.new(username: "DVOG", token:"5444kadib95n509x", uid: 12412401)
+      expect(user).to be_valid
+    end
+  end
 end
