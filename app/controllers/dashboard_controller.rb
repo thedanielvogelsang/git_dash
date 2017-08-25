eb6f0121386c3b07648891f9184a12ee3d376d1f
thedanielvogelsang <dr.vogelsang26@gmail.com>
2017-08-24 18:55:52 -0600

    adds dashboard index route, create user in sessions controller after oauth process, creates session[:user_id] and redirects successfully to dashboard index

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+
+  def index
+    byebug
+  end
+
+end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
   def create
-    byebug
+    @response = Faraday.post("https://github.com/login/oauth/access_token?client_id=#{ENV['github_client_id']}&client_secret=#{ENV['github_client_secret']}&code=#{params['code']}")
+    token = @response.body.split(/\W+/)[1]
+    oauth_response = Faraday.get("https://api.github.com/user?access_token=#{token}")
+    auth = JSON.parse(oauth_response.body)
+    user = User.find_or_create_by(uid: auth['uid'])
+    user.uid = auth['uid']
+    user.username = auth['login']
+    user.token = token
+    user.image = auth['avatar_url']
+    user.save
+    session[:user_id] = user.id
+    redirect_to dashboard_index_path
   end
 end
